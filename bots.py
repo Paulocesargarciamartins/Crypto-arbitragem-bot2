8cc2d5c89cf2091e13ea97bad83ae915219d503e
Paulocesargarciamartins <53310136+Paulocesargarciamartins@users.noreply.github.com>
2025-08-17 16:39:16 -0300

    Update bots.pyteste

--- bots.py
+++ bots.py
@@ -635,3 +635,5 @@ if __name__ == "__main__":
         run_all_bots()
     else:
         run_all_bots()
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
